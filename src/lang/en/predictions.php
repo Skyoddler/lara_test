ef83916bfec902d841bc057068ecb79e3e890b04
skyoddler <skyoddler@outlook.com>
2024-07-24 12:54:22 +0300

    Prediction 1.0

---
+++ src/lang/en/predictions.php
@@ -0,0 +1,5 @@
+<?php
+
+return [
+    'empty_new' => "Field can't be empty",
+];
--- src/resources/views/predictions/index.blade.php
+++ src/resources/views/predictions/index.blade.php
@@ -1,22 +1,34 @@
-<x-guest-layout>
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            {{ __('Main Page') }}
+        </h2>
+    </x-slot>
 
-    @if (@isset($prediction))
-        <x-input-label value="{{ $prediction }}"/>
-    @endif
+    <div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-gray-100">
+        <div class="w-full sm:max-w-md mt-6 px-6 py-4 bg-white shadow-md overflow-hidden sm:rounded-lg">
+            @if (@isset($prediction))
+                <x-input-label value="{{ $prediction }}"/>
+            @endif
 
-    <div class="flex items-center justify-end mt-4">
-        <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-           href="{{ route('login') }}">
-            {{ __('Log in') }}
-        </a>
+            <div class="flex items-center justify-end mt-4">
 
-        <form method="GET" action="{{ route('predictions.index') }}">
-            {{--    @csrf--}}
+                <form method="GET" action="{{ route('predictions.create') }}">
+                    @csrf
 
-            <x-primary-button class="ml-3">
-                {{ __('Get Prediction') }}
-            </x-primary-button>
-        </form>
+                    <x-primary-button class="ml-3">
+                        {{ __('Add Prediction') }}
+                    </x-primary-button>
+                </form>
+
+                <form method="GET" action="{{ route('predictions.index') }}">
+                    @csrf
+
+                    <x-primary-button class="ml-3">
+                        {{ __('Get Prediction') }}
+                    </x-primary-button>
+                </form>
+            </div>
+        </div>
     </div>
-
-</x-guest-layout>
+</x-app-layout>
