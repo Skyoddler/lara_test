44539cf256c330fc1899a912f22d97a7d8e5da14
skyoddler <skyoddler@outlook.com>
2024-07-29 18:11:06 +0300

    composer update

--- src/app/Services/PredictionsService.php
+++ src/app/Services/PredictionsService.php
@@ -4,7 +4,6 @@ namespace App\Services;
 
 use App\Models\Predictions;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\DB;
 use Illuminate\Validation\ValidationException;
 
 class PredictionsService
@@ -14,42 +13,18 @@ class PredictionsService
      */
     public function getRandomPrediction()
     {
-        $model = DB::table('predictions')->inRandomOrder()->first();
-        $data = '';
+        $data = Predictions::query()->inRandomOrder()->first();
 
-        if (isset($model)) {
-            $data = $model->data;
-        }
-
-        return $data;
+        return $data->data ?? '';
     }
 
     public function createPredictionFromRequest(Request $request): Predictions
     {
-        $this->validateRequest($request);
-
         $prediction = new Predictions();
         $prediction->data = $request->get('new_prediction');
-        $prediction->created_at = now();
-        $prediction->updated_at = now();
 
         $prediction->save();
 
         return $prediction;
-    }
-
-    /**
-     * @param Request $request
-     * @return void
-     */
-    private function validateRequest(Request $request)
-    {
-        $data = $request->request->all();
-
-        if (!isset($data['new_prediction']) || $data['new_prediction'] === "") {
-            throw ValidationException::withMessages([
-                'new_prediction' => trans('predictions.empty_new'),
-            ]);
-        }
     }
 }
